dd7c996035b31261a7820cb75ed57301e48a81b7
aristo <dkfud2121@naver.com>
2022-08-08 22:17:12 +0900

    study: Query object as parameter : https://blog.naver.com/dkfud2121/222843299878

--- ChatProject/main.py
+++ ChatProject/main.py
@@ -1,5 +1,5 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -12,13 +12,18 @@ class User(BaseModel):
     id: int
     name: str
     password: str
-    desc : str
+    desc : Union[str, None] = None
 
 @app.get("/users/{user_id}")
-def read_user(user_id: int, q: Union[str, None]=None):
-    return {"user_id" : user_id
-            , "q":q}
+def read_user(user_id: int, q: Union[str, None] = Query(
+    default=None, min_length=3, max_length=50, regex="^fixedquery"
+)):
+    return {"user_id" : user_id, "q":q}
 
 @app.put("/users/{user_id}")
 def upgrade_user(user_id:int, user:User):
     return {"user_name": user.name, "user_id": user.id}
+
+@app.post("/create/users")
+async def create_user(user:User):
+    return user
