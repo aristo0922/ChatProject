c34a61856bf1ed601c6c2337a528c96716b06d4a
aristo <dkfud2121@naver.com>
2022-08-21 01:10:38 +0900

    feat: use dependcy and modulization

--- ChatProject/sql_app/crud.py
+++ ChatProject/sql_app/crud.py
@@ -15,7 +15,7 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
 
 
-def create_user(db: Session, user: schemas.UserCreate, index:int):
+def create_user(db: Session, user: schemas.UserCreate):
     fake_hashed_password = user.password + "notreallyhashed"
     db_user = models.User(email=user.email,
                           user_name=user.user_name,
---
+++ ChatProject/sql_app/routers/users.py
@@ -0,0 +1,20 @@
+# create an instance
+from fastapi import APIRouter
+
+# APIRouter as a mini fastapi class
+router = APIRouter()
+
+
+@router.get("/users/", tags=["users"])
+async def read_users():
+    return [{"username": "Rick"}, {"username": "Morty"}]
+
+
+@router.get("/users/me", tags=["users"])
+async def read_user_me():
+    return {"username": "fakecurrentuser"}
+
+
+@router.get("/users/{username}", tags=["users"])
+async def read_user(username: str):
+    return {"username": username}
